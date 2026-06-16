345e50564a39f78967eae66fc4ee370587313d29
TeresaRojas <tereros29@gimal.com>
2026-06-16 13:10:35 -0600

    Modelos

---
+++ models/prestamo.py
@@ -0,0 +1,19 @@
+from models.libro
+class Prestamo:
+
+    #Cpnstructor
+    def __init__(self, id, libro, usuario, fecha_prestamo, fecha_devolución, estado):
+        self.id = id
+        self.libro = libro
+        self.usuario = usuario
+        self.fecha_prestamo = fecha_prestamo
+        self.fecha_devolución = fecha_devolución
+        self.estado = estado 
+
+    def registrar_devolución(self, fecha_devolución):
+        self.fecha_devolución = fecha_devolución
+        self.estado = "Devuelto"
+        self.libro.devolver()
+
+
+
